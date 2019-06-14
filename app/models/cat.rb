fcbc6f1471454b7c88aa9b8023d422cb6dd37008
howardbdev <howarddevennish@gmail.com>
2019-06-14 11:05:16 -0400

    playing with tests

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,7 @@
 class Cat < ApplicationRecord
+  validates :name, presence: true
+
+  def self.first_liked_cat
+    where(status: "liked").first
+  end
 end
--- test/models/cat_test.rb
+++ test/models/cat_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class CatTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test ".first_liked_cat returns a cat with a status of 'liked'" do
+
+    Cat.create(name: "Fluffy", status: "liked")
+    first_liked = Cat.first_liked_cat
+
+    assert first_liked.status == "liked"
+  end
 end
